faf7fe914a7dd95f7582c3cc8c9ba480ddec613c
MeFTaH <berkayhosgor@gmail.com>
2023-11-09 14:45:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const todoList = document.getElementById("todo-list");
     const removeTaskButton = document.getElementById("remove-task");
     const resetListButton = document.getElementById("reset-list");
+	const addUserButton = document.getElementById("add-user");
+    const removeUserButton = document.getElementById("remove-user");
 
     // Enable input and button when a user is selected
     userSelect.addEventListener("change", function () {
@@ -35,26 +37,61 @@ document.addEventListener("DOMContentLoaded", function () {
             taskInput.value = ""; // Clear the input field
         }
     });
+	 let removeMode = false;
 
     // Handle "Remove Task" button click
-    removeTaskButton.addEventListener("click", function () {
-        const selectedUser = userSelect.value;
-        const selectedItems = Array.from(todoList.children).filter((item) =>
-            item.classList.contains("completed")
-        );
-        selectedItems.forEach((item) => item.remove());
-        saveTodoList(selectedUser);
+removeTaskButton.addEventListener("click", function () {
+        removeMode = !removeMode; // Toggle remove mode
+        updateRemoveMode();
     });
+	   // Directly handle click event on list items
+    todoList.addEventListener("click", function (event) {
+        if (removeMode) {
+            const clickedItem = event.target;
+            if (clickedItem.tagName === "LI") {
+                clickedItem.remove();
+                saveTodoList(userSelect.value);
+            }
+        } else {
+            // Handle other logic (e.g., marking as completed)
+        }
+    });
+	// Function to update the UI based on remove mode
+    function updateRemoveMode() {
+        if (removeMode) {
+            removeTaskButton.textContent = "Exit Remove Mode";
+			alert("Remove Mode Activated!");
+        } else {
+            removeTaskButton.textContent = "Remove Task";
+			alert("Remove Mode Deactivated!");
+        }
+    }
 
     // Handle "Reset List" button click
     resetListButton.addEventListener("click", function () {
         const selectedUser = userSelect.value;
         const confirmation = confirm("Are you sure you want to reset the to-do list?");
         if (confirmation) {
-            clearTodoList();
+            resetCompletedStatus();
             saveTodoList(selectedUser);
         }
     });
+	  // Handle "Add User" button click
+    addUserButton.addEventListener("click", function () {
+        const newUserName = prompt("Enter the name for the new user:");
+        if (newUserName !== null && newUserName.trim() !== "") {
+            addUser(newUserName.trim());
+        }
+    });
+	    // Handle "Remove User" button click
+    removeUserButton.addEventListener("click", function () {
+        const selectedUser = userSelect.value;
+        const confirmation = confirm(`Are you sure you want to remove the user "${selectedUser}"?`);
+        if (confirmation) {
+            removeUser(selectedUser);
+        }
+    });
+
 
     // Directly handle click event on list items
     todoList.addEventListener("click", function (event) {
@@ -76,6 +113,15 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     // Function to clear the to-do list
+	function resetCompletedStatus(selectedUser) {
+    const tasks = Array.from(todoList.children);
+    tasks.forEach((task) => {
+        if (task.classList.contains("completed")) {
+            task.classList.remove("completed");
+        }
+    });
+}
+	
     function clearTodoList() {
         todoList.innerHTML = "";
     }
@@ -104,4 +150,34 @@ document.addEventListener("DOMContentLoaded", function () {
         const storedTasks = localStorage.getItem(user);
         return storedTasks ? JSON.parse(storedTasks) : [];
     }
+	
+	// Function to add a new user
+    function addUser(userName) {
+        const option = document.createElement("option");
+        option.value = userName;
+        option.textContent = userName;
+        userSelect.appendChild(option);
+        userSelect.value = userName;
+        clearTodoList();
+        taskInput.removeAttribute("disabled");
+        addTaskButton.removeAttribute("disabled");
+        removeTaskButton.removeAttribute("disabled");
+        resetListButton.removeAttribute("disabled");
+        saveTodoList(userName);
+    }
+
+    // Function to remove a user
+    function removeUser(userName) {
+        const optionToRemove = Array.from(userSelect.options).find(option => option.value === userName);
+        if (optionToRemove) {
+            userSelect.removeChild(optionToRemove);
+            clearTodoList();
+            taskInput.setAttribute("disabled", "true");
+            addTaskButton.setAttribute("disabled", "true");
+            removeTaskButton.setAttribute("disabled", "true");
+            resetListButton.setAttribute("disabled", "true");
+            localStorage.removeItem(userName);
+        }
+    }
+	
 });
